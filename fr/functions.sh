d6a0834ff53c361ea6833f185b0b749a10cc6b8f
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-25 22:44:03 +0200

    plugin: Add command

--- fr/functions.sh
+++ fr/functions.sh
@@ -1,17 +1,11 @@
-#!/bin/bash
-# Here you can define translations to be used in the plugin functions file
-# the below code is an sample to be reused:
-# 1) uncomment to function below
-# 2) replace XXX by your plugin name (short)
-# 3) remove and add your own translations
-# 4) you can the arguments $2, $3 passed to this function
-# 5) in your plugin functions.sh file, use it like this:
-#      say "$(pv_myplugin_lang the_answer_is "oui")"
-#      => Jarvis: La réponse est oui
+#!/usr/bin/env bash
 
-#pv_XXX_lang () {
-#    case "$1" in
-#        i_check) echo "Je regarde...";;
-#        the_answer_is) echo "La réponse est $2";;
-#    esac
-#} 
+jv_pg_fb_lang()
+{
+  case "$1" in
+    send_message_success) echo "Message facebook envoyé à $2";;
+    send_message_failed) echo "Echec de l'envoi du message facebook à $2";;
+
+    *) jv_error "Erreur: Clef de traduction '$1' non reconnue. Merci de contacter le concepteur de ce plugin.";;
+  esac
+}
