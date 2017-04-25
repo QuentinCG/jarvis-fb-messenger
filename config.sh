f68a4f5419ce8e6d54471d8838a3114072dd36fa
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-25 14:33:23 +0200

    config: Add base config for facebook chat plugin

---
+++ config.sh
@@ -0,0 +1,5 @@
+var_jv_pg_fb_username="YOUR USERNAME HERE" # Facebook username (used by Jarvis to send/receive messages/images)
+var_jv_pg_fb_password="YOUR PASSWORD HERE" # Facebook password
+
+var_jv_pg_fb_allow_all_control="True" # Allow all people to control Jarvis (True/False)
+var_jv_pg_fb_allowed_people_ids="[todo, todo2, todo3, ...]" # List of allowed people (facebook IDs) to control Jarvis
