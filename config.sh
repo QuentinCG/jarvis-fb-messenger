183993fadbce2b975d35cbb4ca87e6d14f1298c8
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-26 22:46:40 +0200

    plugin: Add readibility on the configuration of this plugin

--- config.sh
+++ config.sh
@@ -1,9 +1,16 @@
+# Base configuration (mandatory)
 var_jv_pg_fb_email="YOUR EMAIL ADDRESS HERE" # Facebook email used as username (used by Jarvis to send/receive messages/image)
 var_jv_pg_fb_password="YOUR PASSWORD HERE" # Facebook password
+
+# Security configuration
+var_jv_pg_fb_allow_control_to_all="False" # Allow all people to control Jarvis (True/False)
+var_jv_pg_fb_allowed_people_ids="[\"id_1_here\", \"id_2_here\", \"id_3_here\", \"...\"]" # List of allowed people (facebook IDs) to control Jarvis
+
+# Used to send a messenger message from the command file or an other plugin (not needed for basic use of this plugin)
+var_jv_pg_fb_main_receiver_id="YOUR RECEIVER ID HERE" # User which will receive message from Jarvis (if configured in command file or in other plugin with this key)
+var_jv_pg_fb_main_receiver_name="YOUR RECEIVER NAME HERE"
+
+# Optional configuration
 var_jv_pg_fb_verbose="True" # Have verbose information (True/False)
 var_jv_pg_fb_mute="True" # Speak with Jarvis without sound (True/False)
 var_jv_pg_fb_getId="True" # Send sender ID back to user which sent a message (useful to get list of people to allow later with var_jv_pg_fb_allowed_people_ids)
-
-# Not used yet:
-var_jv_pg_fb_allow_control_to_all="True" # Allow all people to control Jarvis (True/False)
-var_jv_pg_fb_allowed_people_ids="[\"id_1_here\", \"id_2_here\", \"id_3_here\", \"...\"]" # List of allowed people (facebook IDs) to control Jarvis
