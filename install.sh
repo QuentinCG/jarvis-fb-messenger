b9ff973597e595fb4022cab1be2a189d8b49ebab
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-27 00:29:02 +0200

    install: Improve install to be faster and with less human interaction

--- install.sh
+++ install.sh
@@ -4,23 +4,23 @@
 # and then do a "sudo apt-get update && sudo apt-get upgrade"
 # and start the install again
 
-# Install Python (python 2.7 and 3.x)
-[[ -z $(which python) ]] && sudo apt-get install python
-[[ -z $(which python2.7) ]] && sudo apt-get install python2.7
-[[ -z $(which python3) ]] && sudo apt-get install python3
+# Install Python (python 3.x)
+[[ -z $(which python) ]] && sudo apt-get --yes --force-yes install python
+[[ -z $(which python3) ]] && sudo apt-get --yes --force-yes install python3
+#[[ -z $(which python2.7) ]] && sudo apt-get --yes --force-yes install python2.7
 
 # Install pip
-[[ -z $(which python-pip) ]] && sudo apt-get install python-pip
-[[ -z $(which python3-pip) ]] && sudo apt-get install python3-pip
+[[ -z $(which python-pip) ]] && sudo apt-get --yes --force-yes install python-pip
+[[ -z $(which python3-pip) ]] && sudo apt-get --yes --force-yes install python3-pip
 
 # Install libs used by fbchat (this may take some time since it needs to build libs)
-[[ -z $(which libxml2-dev) ]] && sudo apt-get install libxml2-dev
-[[ -z $(which libxslt1-dev) ]] && sudo apt-get install libxslt1-dev
-[[ -z $(which libxml2) ]] && sudo apt-get install libxml2
+[[ -z $(which libxml2-dev) ]] && sudo apt-get --yes --force-yes install libxml2-dev
+[[ -z $(which libxslt1-dev) ]] && sudo apt-get --yes --force-yes install libxslt1-dev
+[[ -z $(which libxml2) ]] && sudo apt-get --yes --force-yes install libxml2
 
 # Install fbchat (needed for the jarvis-facebook messenger python script)
-sudo pip2.7 install fbchat
 sudo pip3 install fbchat
+#sudo pip2 install fbchat
 
 # Copy latest version of "facebook messenger sender" python script
 wget -P script/ https://raw.githubusercontent.com/QuentinCG/Base-Scripts/master/OS_Independent/utils/fb_messenger_send.py
