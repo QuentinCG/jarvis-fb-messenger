ece79d80a1654fbedcc1fc4e0d516d6c5fabd762
Quentin Comte-Gaz <quentin@comte-gaz.com>
2019-02-05 13:52:26 +0100

    Config: Help user get the plugin work (first use)
    This should fix issue #10

--- config.sh
+++ config.sh
@@ -12,4 +12,4 @@ var_jv_pg_fb_main_receiver_name="YOUR RECEIVER NAME HERE"
 
 # Optional configuration
 var_jv_pg_fb_mute="True" # Speak with Jarvis without sound (True/False)
-var_jv_pg_fb_getId="False" # Send sender ID back to user which sent a message (useful to get list of people to allow later with var_jv_pg_fb_allowed_people_ids)
+var_jv_pg_fb_getId="True" # Send sender ID back to user which sent a message (useful to get list of people to allow later with var_jv_pg_fb_allowed_people_ids, <!> Put this to False once you know your id)
